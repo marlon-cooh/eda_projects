1c23849fca4e7076a5c1f42a216bf6dc942a6cde
Marlon Marin <marlonmariin97@outlook.com>
2024-06-18 14:38:57 -0500

    (build) 2-pib.ipynb, library utils/cleaning was included. (build) Fixes in utils/cleaning.py

--- utils/cleaning.py
+++ utils/cleaning.py
@@ -6,21 +6,18 @@ class Drop:
     def __init__(self, pandas_obj) -> pd.DataFrame:
         self.pandas_obj = pandas_obj
         
-    def drop_cols(cols, pandas_obj):
-        return pandas_obj.drop(cols, axis=1, inplace=True)
+    def drop_cols(self, cols):
+        return self.pandas_obj.drop(cols, axis=1, inplace=True)
         
-    def convert_figures(cols, pandas_obj, reg=","):
+    def convert_figures(self, cols, reg=","):
         """
             convert_figures aids to replace values as: "1,234,567" to 1234567, in this case, final value is divided in $4127.50 (TRM COP->USD, 18th June 2024)
             arguments:
-            `cols`: column value to be converted
-            `reg` : regular expression to replace, by default = ","
-            `pandas_obj` : original raw dataframe.
-            
+            `cols`: column value to be converted,
+            `reg` : regular expression to replace, by default = ","        
         """
-        df = Drop.drop_cols(cols, pandas_obj)
         for col in cols:
-            df[col] = np.array([( int(re.sub(reg, "", i)) / 4127.50 ) for i in df[col].values])
+            self.pandas_obj[col] = self.pandas_obj[col].apply(lambda x: int(re.sub(reg, "", str(x))) / 4127.50)
 
 def run():
     pass
