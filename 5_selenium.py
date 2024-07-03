e422c972b03b800ad18b1f7b6fbc232f444dad89
Marlon Marin <marlonmariin97@outlook.com>
2024-07-03 15:07:11 -0500

    (build) essays with beautifulsoup

--- 5_selenium.py
+++ 5_selenium.py
@@ -1,6 +1,25 @@
 #!/usr/bin/env python3
 
+from selenium import webdriver # type: ignore
+import pandas as pd
+import os
+
+def testing():
+    path = "/usr/bin/chromedriver"
+    try:
+        if os.path.exists(path):
+            
+            cService = webdriver.ChromeService(executable_path = path)
+            driver = webdriver.Chrome(service = cService)
+            driver.get("https://www.facebook.com/dannia.okampo")
+            print(driver.title)
+            driver.quit()
+    except AttributeError:
+        print("This caremonda raises 'str' object has no attribute 'capabilities'")
+    
+
 def run():
-    print("Hello, world!")
+    testing()
+    
 if __name__ == "__main__":
     run()
